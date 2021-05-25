4de8bd81d972a3ac087893320d1ff69bdd6988b8
jefer <jefmax89@gmail.com>
2021-05-25 09:57:54 -0500

    first commit

---
+++ js/arepa.js
@@ -0,0 +1,31 @@
+
+// $(document).ready(function() {  
+// 	 $('#boton-receta').click(function(){
+//         var imagen_old = $('#cambio').replaceWith('<img id="cambio" src="img/arepas/katir.jpg" alt="imagen cambiada">');
+//         $('body').append(imagen_old);
+// 	 });
+// });
+
+
+// $(document).ready(function() {  
+//     $('#boton-cambio').click(function(){
+//        $('#cambio').replaceWith('<img id="cambio" src="blue.jpeg" alt="imagen cambiada" width="500" height="400">');
+//     });
+// });
+
+
+//funciona
+// $(document).ready(function() {
+// $("#boton-receta").click(function(){
+//     $('#cambio').fadeToggle('#cambiod');
+//   });
+// });  
+
+
+
+
+// $("h1").hover( function(){
+//     $(this).animate({"opacity": "0.5"}, 500);
+//   }, function(){
+//     $(this).animate({"opacity": "1"}, 500);
+//   });
---
+++ js/formulario.js
@@ -0,0 +1,3 @@
+function myFunction() {
+    alert("Gracias por tus comentarios, trabajamos para darte siempre lo mejor!");
+  }
